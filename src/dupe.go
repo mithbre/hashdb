629db0e0ddd9ad60d91d95d3a3d6149b2a5a0d05
Calvin Hansen <TheSilentGhost@gmail.com>
2017-07-10 17:17:05 -0400

    Print a list of all files with duplicates.

--- src/dupe.go
+++ src/dupe.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "path/filepath"
     "fmt"
     "os"
     "database/sql"
@@ -9,50 +8,30 @@ import (
 
 )
 
-func trackedFileDupe(db *sql.DB, file string, path string) {
-    var fid string
-    var sha1 []byte
-    var sha2 []byte
-    var dfile string
-    var dpath string
-
-    absFile, _ := filepath.Abs(file)
-    absPath, _ := filepath.Abs(path)
-
-    fi := filepath.Clean(absFile)
-    pa := filepath.Clean(absPath)
-
-    fiPath, filename := filepath.Split(fi)
-    fiPath = filepath.Clean(fiPath)
-
-    err := db.QueryRow(
-        `SELECT id_file, sha1, sha256
-         FROM tblFile
-         INNER JOIN tblPath
-         ON tblFile.path = tblPath.id_path
-         WHERE tblPath.path = ?
-         AND tblFile.filename = ?`, fiPath, filename).Scan(&fid, &sha1, &sha2)
-
-    switch {
-    case err == sql.ErrNoRows:
-        fmt.Printf("File NOT found.\n")
+func SelectDupes() {
+    db, err := DBInit("../test.db")
+    if err != nil {
+        fmt.Println(err)
         os.Exit(1)
-    case err != nil:
-        fmt.Printf("%s\n", err)
-        os.Exit(1)
-    default:
-        fmt.Printf("Found File.\n")
     }
 
+    var dfile string
+    var dpath string
+    var id int
+    var leng int
+    var prev int
+
     res, err := db.Query(
-        `SELECT filename, tblPath.path
-         FROM tblFile
-         INNER JOIN tblPath
-         ON tblFile.path = tblPath.id_path
-         WHERE tblPath.path LIKE ?
-         AND sha1 = ?
-         AND sha256 = ?
-         AND id_file != ?`, pa+"%", sha1, sha2, fid)
+       `SELECT id_file, filename, tblPath.path, leng
+        FROM tblFile x, tblPath
+        JOIN (
+            SELECT sha1, sha256
+            FROM tblFile
+            GROUP BY sha1, sha256
+            HAVING COUNT (*) > 1) y
+        ON y.sha1 = x.sha1 AND y.sha256 = x.sha256
+        WHERE x.path = tblpath.id_path
+        ORDER BY leng`)
 
     if err != nil {
         fmt.Println(err)
@@ -62,12 +41,17 @@ func trackedFileDupe(db *sql.DB, file string, path string) {
 
     fmt.Printf("Results:\n")
     for res.Next() {
-        err := res.Scan(&dfile, &dpath)
+        err := res.Scan(&id, &dfile, &dpath, &leng)
         switch err {
         case sql.ErrNoRows:
            fmt.Printf("No rows returned.\n")
         default:
-            fmt.Println(dfile, dpath)
+            if leng != prev {
+                fmt.Println("\n")
+            }
+            prev = leng
+            fmt.Println(id, dfile, dpath)
+
         }
     }
 }
